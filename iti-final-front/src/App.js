2b83aba26bd89790b8678495839a359ffa73c529
Ahmed Gad <ahmed.gad19@hotmail.com>
2023-05-26 19:58:59 +0300

    navbar creation

--- iti-final-front/src/App.js
+++ iti-final-front/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-
-
+import Navbar from '../src/Components/Navbar/Navbar'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div>
-    </div>
+  <>
+  <Navbar/>
+
+  </>
   );
 }
 
--- iti-final-front/src/Components/Navbar/Navbar.js
+++ iti-final-front/src/Components/Navbar/Navbar.js
@@ -1,70 +1,38 @@
-import React from 'react';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { useRef, React } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../../images/main/logo.png";
+import { NavLink } from "react-router-dom";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./navbar.css";
 
-function Navbar() {
+export default function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  };
+
   return (
-    <div>
-<nav className="navbar navbar-expand-lg position-fixed ">
-    <div className="container ">
-        <a className="navbar-brand offset-lg-4" href="#">
-        <img src="./assets/images/logo-removebg-preview.png" alt="" />
-        </a>
-        <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        >
-        <span className="navbar-toggler-icon" />
+    <header className="container-fluid">
+      <div className="container">
+        <NavLink className='logo'>
+          <img src={Logo} alt="logo" />
+        </NavLink>
+        <nav ref={navRef}>
+          <NavLink className="links">الرئيسية</NavLink>
+          <NavLink className="links">تسوقي</NavLink>
+          <NavLink className="links">عنا</NavLink>
+          <NavLink className="links">المدونه</NavLink>
+          <NavLink className="links register-btn">التسجيل</NavLink>
+          <NavLink className="links login-btn"> دخول</NavLink>
+          <button className="nav-btn nav-btn-close" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar me-auto mb-2 mb-lg-0 ">
-            <li className="nav-item">
-            <a className="nav-link active__nav" aria-current="page" href="#">
-                الرئيسية
-            </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="./assets/mother-profile.html">
-                {" "}
-                صفحتك
-            </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="assets/product.html">
-                تسوقي
-            </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="#">
-                عنا
-            </a>
-            </li>
-            <li className="nav-item">
-            <a className="nav-link" href="assets/blog.html">
-                المدونة
-            </a>
-            </li>
-            <li className="nav-item items__btn">
-            <a className="nav-link primary_btn" href="assets/join us.html">
-                تسجيل
-            </a>
-            </li>
-            <li className="nav-item items__btn">
-            <a className="nav-link secondary_btn" href="assets/signin.html">
-                دخول
-            </a>
-            </li>
-        </ul>
-        </div>
-    </div>
-</nav>
-
-    </div>
-  )
+      </div>
+    </header>
+  );
 }
-
-export default Navbar;
