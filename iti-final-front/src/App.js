c8779306f206bf01616c153a8205dba9d93d51de
Mohamed Khaled <73159571+MohamedElattar11@users.noreply.github.com>
2023-05-26 21:06:57 +0300

    ##

--- iti-final-front/src/App.js
+++ iti-final-front/src/App.js
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-=======
+
 import logo from './logo.svg';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Products from './Pages/Products';
-import Blog from './Pages/Blog';
-
-
->>>>>>> 62d17d824e50c1d00569d56bd837c21e4d29817f
-
 import Navbar from '../src/Components/Navbar/Navbar'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
 function App() {
   return (
-<<<<<<< HEAD
+
   <>
   <Navbar/>
-
   </>
-=======
-    <div>
-      
-    </div>
->>>>>>> 62d17d824e50c1d00569d56bd837c21e4d29817f
   );
 }
-
 export default App;
