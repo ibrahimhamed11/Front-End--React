f2d5486297b385abb57f1d00116cd424b194efa9
MoahmedKhaled11 <mka20182019@gmail.com>
2023-06-04 14:40:56 +0300

    Responsive about us page

--- iti-final-front/src/Pages/About.js
+++ iti-final-front/src/Pages/About.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import AboutImage from  '../images/main/a-mom-and-dad-parent-kissing-their-young-baby-1024x684.jpg';
+import './about.css';
+
 
 export default function About() {
   return (
-    <div>About</div>
+    
+    <div className="container">
+        <div className='about-content'>
+          <div className='about-top'>
+            <p>نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.</p>
+            <img src={AboutImage}/>
+          </div>
+          <div className='about-bottom'>
+          <img src={AboutImage}/>
+          <p>نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.نقدم لكِ خدمات لمتابعة صحة و نمو طفلك.تابعينا و إنضمي إلينا و إضمني صحة طفلك.</p>
+          </div>
+        </div>
+    </div>
   )
 }
